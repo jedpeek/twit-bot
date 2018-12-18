564c8bea2a6cd930a5fa5bf26e6024b2f9426d2d
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-12-18 09:13:05 -0800

    removed scripts

--- scripts/create-env.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `
-CONSUMER_KEY = ${process.env.CONSUMER_KEY}\n,
-CONSUMER_SECRET = ${process.env.CONSUMER_SECRET}\n,
-ACCESS_TOKEN = ${process.env.ACCESS_TOKEN}\n,
-ACCESS_TOKEN_SECRET = ${process.env.ACCESS_TOKEN_SECRET}\n
-`)
